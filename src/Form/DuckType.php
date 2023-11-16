7c8e2f55216fb96854ca6b2f4d08dc79ba31d14a
Lucas HOANG <lucas.hoang@le-campus-numerique.fr>
2023-11-16 09:20:54 +0100

    ADD Form class to create an account

---
+++ src/Form/DuckType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Duck;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DuckType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, ['label' => 'Firstname'])
+            ->add('lastname', TextType::class, ['label' => 'Lastname'])
+            ->add('duckname', TextType::class, ['label' => 'Duckname'])
+            ->add('email', TextType::class, ['label' => 'email'])
+            ->add('password', PasswordType::class, ['label' => 'password'])
+            ->add('save', SubmitType::class, ['label' => 'Create'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Duck::class,
+        ]);
+    }
+}
