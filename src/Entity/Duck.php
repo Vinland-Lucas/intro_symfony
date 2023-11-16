09eefc1b876f07034660d82d4cc35c1220a6f65a
Lucas HOANG <lucas.hoang@le-campus-numerique.fr>
2023-11-16 09:18:58 +0100

    ADD Validators in Duck Class

--- src/Entity/Duck.php
+++ src/Entity/Duck.php
@@ -5,6 +5,9 @@ namespace App\Entity;
 use App\Repository\DuckRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints\PasswordStrength;
+use Symfony\Component\Validator\Constraints\PasswordStrengthValidator;
 
 #[ORM\Entity(repositoryClass: DuckRepository::class)]
 class Duck
@@ -14,18 +17,33 @@ class Duck
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Assert\NotBlank]
     #[ORM\Column(type: Types::TEXT)]
     private ?string $firstname = null;
 
+    #[Assert\NotBlank]
     #[ORM\Column(type: Types::TEXT)]
     private ?string $lastname = null;
 
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 20)]
     #[ORM\Column(type: Types::TEXT)]
     private ?string $duckname = null;
 
+    #[Assert\NotBlank]
+    #[Assert\Email(
+        message: 'The email {{ value }} is not a valid email.',
+    )]
     #[ORM\Column(type: Types::TEXT)]
     private ?string $email = null;
 
+    #[Assert\NotBlank]
+    #[Assert\PasswordStrength([
+        'minScore' => PasswordStrength::STRENGTH_VERY_STRONG,
+    ])]
+    #[Assert\AtLeastOneOf([
+        new Assert\Length(min: 10),
+    ])]
     #[ORM\Column(type: Types::TEXT)]
     private ?string $password = null;
 
