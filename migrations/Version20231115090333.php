449fec9fb63d1a980b7a9605e93407bdd068af26
Lucas HOANG <lucas.hoang@le-campus-numerique.fr>
2023-11-16 09:22:59 +0100

    ADD Migrations(la table duck dans la BDD je pense)

---
+++ migrations/Version20231115090333.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231115090333 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE duck (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, firstname CLOB NOT NULL, lastname CLOB NOT NULL, duckname CLOB NOT NULL, email CLOB NOT NULL, password CLOB NOT NULL)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE duck');
+    }
+}
